76c491e4963bf8dc14b710fc7c8798225d7af7f5
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2018-01-05 17:27:23 +0300

    local changes

--- sampling/metabolitics_sampling.py
+++ sampling/metabolitics_sampling.py
@@ -1,8 +1,13 @@
 from metabolitics.analysis import MetaboliticsAnalysis
-from cobra.flux_analysis.sampling import OptGPSampler, ACHRSampler
+from cobra.flux_analysis.sampling import OptGPSampler
 
 
 class MetaboliticsSampling(MetaboliticsAnalysis):
+
     def sampling_analysis(self, measurements):
-        self.add_constraint(measurements)
-        return OptGPSampler(self.model, processes=24).sample(50000)
+        # self.add_constraint(measurements)
+
+        import pdb
+        pdb.set_trace()
+
+        return OptGPSampler(self.model, processes=1).sample(50000)
--- sampling/scripts.py
+++ sampling/scripts.py
@@ -1,8 +1,7 @@
 import json
-from time import time
 
 import click
-from joblib import Parallel, delayed
+# from joblib import Parallel, delayed
 from sklearn_utils.utils import SkUtilsIO
 from metabolitics.preprocessing import MetaboliticsPipeline
 
@@ -22,7 +21,7 @@ def save_sampling_on_bc():
     y = ['healthy' if i == 'h' else 'bc' for i in y]
 
     def transform(x):
-        analyzer = MetaboliticsSampling('recon2')
+        analyzer = MetaboliticsSampling('ecoli')
         return analyzer.sampling_analysis(x)
 
     pipe = MetaboliticsPipeline(['naming', 'metabolic-standard-scaler'])
@@ -32,3 +31,14 @@ def save_sampling_on_bc():
         for x, label in zip(X_t, y):
             d = to_flat_df(transform(x))
             f.write('%s\n' % json.dumps([label, d]))
+
+
+@cli.command()
+def sampling_diff():
+    X, y = SkUtilsIO('../datasets/diseases/BC.csv').from_csv(
+        label_column='stage')
+
+    import pdb
+    pdb.set_trace()
+
+    # f = open('/media/muhammedhasan/hdd/sampling_anaylsis_bc.json')
